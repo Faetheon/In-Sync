e4674fa8823de1a63d413f47fac932c15e6768c9
Nathan Hill <nateohill@gmail.com>
2019-07-06 10:25:17 -0700

    Added an aboutNathan property to ReduxStore interface Created a Nathan interface Created a Skill interface

--- react/src/ts-interfaces/interfaces.ts
+++ react/src/ts-interfaces/interfaces.ts
@@ -4,5 +4,18 @@ export interface Item {
 }
 
 export interface ReduxStore {
-  items: Array<Item>
+  items: Array<Item>,
+  aboutNathan: Nathan
+}
+
+export interface Skill {
+  name: string,
+  workExp: number,
+  parent?: string
+}
+
+export interface Nathan {
+  name: string,
+  age: number,
+  skills: Array<Skill>
 }
