b3256d607b2b6459e623932b41ea3cb8b06c2a92
Nathan Hill <nateohill@gmail.com>
2020-01-09 15:08:56 -0500

    Got a basic test working

---
+++ react/src/components/tests/Home.test.js
@@ -0,0 +1,20 @@
+// Configure adapter
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+configure({ adapter: new Adapter() });
+
+// Test
+import React from "react";
+import { shallow, mount, render } from "enzyme";
+
+import Home from "../App/Home.tsx";
+
+test("Returns html string", () => {
+  expect(shallow(<Home />).type("string")).toBe(true);
+});
+
+// // test file
+// import { shallow, mount, render } from "enzyme";
+
+// const wrapper = shallow(<Foo />);
