5bfb9e9b00277d4eb7912a9dc5986c4104ca0e1a
Nathan Hill <nateohill@gmail.com>
2020-01-09 15:17:17 -0500

    Removed cors module and served file to port 3000

--- server/index.js
+++ server/index.js
@@ -1,16 +1,20 @@
 require("dotenv").config();
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const pg = require("pg");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(express.static(`${__dirname}/../react/dist/`));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.search(bodyParser.json());
-app.use(cors());
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../react/dist/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}.`);
