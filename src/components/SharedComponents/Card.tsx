8df2c3fd95274a016019e79618d8c432ed228aa2
Nathan Hill <nateohill@gmail.com>
2020-01-23 16:46:18 -0500

    Added a card component to hold content

---
+++ src/components/SharedComponents/Card.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const CardBody = styled.div`
+  display: flex;
+  border: 4px solid black;
+  border-radius: 5%;
+  width: 90%;
+  height: 90%;
+  box-shadow: 10px 5px 10px grey;
+  margin-top: 6%;
+`;
+
+const Card = () => {
+  return (
+    <CardBody>
+
+    </CardBody>
+  );
+}
+
+export default Card;
