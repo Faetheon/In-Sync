bc0d91938a6b84ac220ad1e636295e8db7857abf
Nathan Hill <nateohill@gmail.com>
2020-01-23 16:48:11 -0500

    Added aliases for SharedComponents and ts-interfaces

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,19 @@
 const path = require("path");
 
-const SRC_DIR = path.join(__dirname, "/react/src");
-const DIST_DIR = path.join(__dirname, "/react/dist");
+const SRC_DIR = path.join(__dirname, "/src");
+const DIST_DIR = path.join(__dirname, "/dist");
 
 module.exports = {
   mode: process.env.MODE || "development",
-  entry: path.join(__dirname, "/react/src"),
+  entry: path.join(__dirname, "/src"),
   output: {
     path: DIST_DIR,
     filename: "bundle.js"
   },
   resolve: {
-    root: path.resolve(__dirname),
     alias: {
-      "@interfaces": path.resolve(__dirname, "/src/ts-interfaces")
+      "@interfaces": path.resolve(__dirname, "/src/ts-interfaces"),
+      "@shared": path.resolve(__dirname, "src/components/SharedComponents")
     },
     enforceExtension: false,
     extensions: [".tsx", ".js", ".ts", ".jsx"]
