dcf82fcd6358ffebfbfaf11cd336afb2195325b5
Nathan Hill <nateohill@gmail.com>
2020-03-03 08:52:15 -0500

    Moved styling to styled components

--- src/components/App/Menu/Home.tsx
+++ src/components/App/Menu/Home.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import Card from 'sharedComponents/Card';
+import styled from 'styled-components';
+
+const HomePage = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 750px;
+`;
 
 const Home = () => {
 
   return (
-    <div className="home">
+    <HomePage>
       <Card />
-    </div>
+    </HomePage>
   );
 }
 
--- src/components/App/Menu/Menu.tsx
+++ src/components/App/Menu/Menu.tsx
@@ -1,5 +1,25 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+import styled from 'styled-components';
+
+const TopBar = styled.div`
+display: flex;
+justify-content: space-around;
+font-size: 20px;
+font-weight: bold;
+margin: 0;
+`;
+
+const TopBarOption = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 0;
+  height: 100px;
+  flex-grow: 1;
+  background-image: linear-gradient(#4d4d4d, #808080, white);
+  justify-content: center;
+  cursor: pointer; 
+`;
 
 interface Menu {
   handleClick: Function
@@ -11,17 +31,17 @@ function Menu(props: any) {
   };
 
   return (
-    <div className='menu'>
-      <div className='home' onClick={() => handleClick('/Home')}>
+    <TopBar>
+      <TopBarOption onClick={() => handleClick('/Home')}>
         Home
-      </div>
-      <div className='apps' onClick={() => handleClick('/Apps')}>
+      </TopBarOption>
+      <TopBarOption onClick={() => handleClick('/Apps')}>
         Apps
-      </div>
-      <div className='about-me' onClick={() => handleClick('/AboutMe')}>
+      </TopBarOption>
+      <TopBarOption onClick={() => handleClick('/AboutMe')}>
         About Me
-      </div>
-    </div>
+      </TopBarOption>
+    </TopBar>
   );
 }
 
