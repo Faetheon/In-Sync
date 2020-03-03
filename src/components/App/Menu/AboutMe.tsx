59af7117374535eae6ebb952f6087b6630bc72ce
Nathan Hill <nateohill@gmail.com>
2020-03-03 11:25:30 -0500

    Added properties to display more information about myself

--- src/components/App/Menu/AboutMe.tsx
+++ src/components/App/Menu/AboutMe.tsx
@@ -10,6 +10,10 @@ const AboutMe = () => {
     <div>
       <div>Me: {aboutNathan.name}</div>
       <div>My mission: {aboutNathan.mission}</div>
+      <div>What I enjoy: {aboutNathan.likes.reduce((acc, curr) => acc += ` ${curr}`, '')}</div>
+      <div>My dream: {aboutNathan.dream}</div>
+      <div>Education: {aboutNathan.education.reduce((acc, curr) => acc += ` ${curr}`, '')}</div>
+      <div>About Me: {aboutNathan.aboutMe}</div>
     </div>
   );
 }
