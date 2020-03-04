da09167369e860eceb4a4f392570e2fcb1bdaad6
Nathan Hill <nateohill@gmail.com>
2020-03-04 12:01:44 -0500

    Added more information about myself

--- src/redux/reducer.ts
+++ src/redux/reducer.ts
@@ -5,15 +5,15 @@ import moment from 'moment';
 
 export const defaultState: ReduxStore = {
   items: [],
-  aboutNathan: {
-    name: "Nathan",
-    age: moment().diff('1999-03-04', 'years'),
-    mission: "My mission is to keep progressing in my skills and knowledge. I learn something new every day whether its a new library or a new language. I find no mountain is too high and though I may fall I won't give up.",
-    likes: ["How to train your dragon franchise", "Dungeon diving games", "Reading", "Finding simple yet effective solutions"],
-    dream: "I want to create or discover new worlds and help others find them through virtual reality, games, or space exploration.",
-    education: ["Highschool diploma", "Coding bootcamp"],
-    aboutMe: "I love my family and am an avid reader. Always on the lookout for new litrpg books I enjoy the ideas that the authors have. I also enjoy gaming and the occasional theological disscussion."
-  }
+  aboutNathan: [
+    "Nathan Hill",
+    `I am ${moment().diff('1999-03-04', 'years')} years old`,
+    "My mission is to keep progressing in my skills and knowledge. I learn something new every day whether its a new library or a new language. I find no mountain is too high and though I may fall I won't give up.",
+    "I like How to train your dragon franchise, Dungeon diving games, Reading, Finding simple yet effective solutions",
+    "My dream is to create or discover new worlds and help others find them through virtual reality, games, or space exploration.",
+    "I have achieved a Highschool diploma and gone through a Coding bootcamp",
+    "I love my family and am an avid reader. Always on the lookout for new litrpg books I enjoy the ideas that the authors have. I also enjoy gaming and the occasional theological disscussion."
+  ]
 };
 
 const rootReducer = (state = defaultState, action: any) => {
